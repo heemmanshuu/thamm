f3cbe8eb8fc18f817406996100700c3391acccc2
klfang1 <ucikevfang2023@gmail.com>
2025-05-27 23:04:58 -0700

    Added Kafka Sink to Flink Job to send matches to game server

--- src/main/java/FlinkMatchmaker.java
+++ src/main/java/FlinkMatchmaker.java
@@ -5,10 +5,10 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
-
+import org.apache.flink.connector.kafka.sink.KafkaSink;
+import org.apache.flink.connector.kafka.sink.KafkaRecordSerializationSchema;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-import java.util.*;
 
 public class FlinkMatchmaker {
 
@@ -26,9 +26,9 @@ public class FlinkMatchmaker {
                 .build();
 
         // 1. Initialize MMRBucketizer with current mean, stddev, k buckets (TODO: mean and stddev should be recalculated at regular intervals as a form of load balancing)
-        double mean = 1000.0;
-        double stddev = 200.0;
-        int k = 20;
+        double mean = 1500.0;
+        double stddev = 500.0;
+        int k = 50;
         MMRBucketizer mmrBucketizer = new MMRBucketizer(mean, stddev, k);
 
         // create a flink DataStream to process player records
@@ -38,11 +38,7 @@ public class FlinkMatchmaker {
                 "Incoming Players"
         );
 
-        jsonStream.print();
-
-        // transform the json stream into stream of maps representing a join_queue request
-        //SingleOutputStreamOperator<Map<String, Object>> players = jsonStream.map(new JsonToMap());
-
+        // transform the json stream into stream of players who sent join requests
         DataStream<Player> players = jsonStream.map(new MapFunction<String, Player>() {
             private final ObjectMapper objectMapper = new ObjectMapper();
 
@@ -59,6 +55,18 @@ public class FlinkMatchmaker {
                 .process(new PlayerMatchmaker());
 
         matches.print();
+
+        // write the matches using a Kafka Sink to a topic that will be processed by the game server
+        KafkaSink<String> sink = KafkaSink.<String>builder()
+                .setBootstrapServers("localhost:9092")
+                .setRecordSerializer(KafkaRecordSerializationSchema.builder()
+                        .setTopic("match-results-1")
+                        .setValueSerializationSchema(new SimpleStringSchema())
+                        .build())
+                .build();
+
+        matches.map(match -> new ObjectMapper().writeValueAsString(match))
+                .sinkTo(sink);
 
         env.execute("Flink Matchmaker with KafkaSource");
     }
